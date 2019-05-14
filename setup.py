d4a70c15b1d37acfd086b99ee0a55ea87b06e89b
oakdream <24935525@qq.com>
2019-05-14 14:58:01 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='tailf',
+    version='1.0',
+    description='This is a test of the setup',
+    author='mlp',
+    author_email='mlp@gmail.com',
+    url='https://github.com/oakdream/tailf.git',
+    packages=['tailf']
+)
